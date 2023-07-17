c692cbe74e58eb46d3e7b1f420357fc97927b9c9
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-07-17 10:39:56 +0800

    Display not found message

--- components/VDictionary.vue
+++ components/VDictionary.vue
@@ -14,7 +14,8 @@
                 </button>
             </form>
         </div>
-        <h1 v-show="!results.length" class="text-center text-4xl font-bold mt-5">Search anything...</h1>
+        <h1 v-show="!results.length && !error" class="text-center text-4xl font-bold mt-5">Search anything...</h1>
+        <h1 v-show="error" class="text-center text-4xl font-bold mt-5">{{ error }}</h1>
         <DictionaryBody v-show="results.length" :results="results" />
     </div>
 </template>
@@ -26,6 +27,7 @@ import DictionaryBody from "./DictionaryBody.vue";
 const word = ref('');
 const results = ref([]);
 const searchElement = ref(null)
+const error = ref(null)
 
 watch(word, (newWord) => {
     results.value = _.isEmpty(newWord) || _.isNil(newWord) ? [] : results.value;
@@ -33,8 +35,11 @@ watch(word, (newWord) => {
 
 const search = () => {
     results.value = [];
+    error.value = null;
     useDictionary(word.value).then(response => {
         results.value = response;
+    }).catch(() => {
+        error.value = `No results found for "${word.value}"`
     });
 }
 
